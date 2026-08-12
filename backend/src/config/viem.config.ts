3b65490587a42f51e6867757ca955550fe315676
Nvoinxv <sukinvoin67@gmail.com>
2026-08-12 08:52:50 +0800

    perubahan kode

---
+++ backend/src/config/viem.config.ts
@@ -0,0 +1,25 @@
+// src/config/viem.config.ts
+import { createPublicClient, createWalletClient, http } from 'viem';
+import { sepolia, hardhat } from 'viem/chains';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const chainId = parseInt(process.env.CHAIN_ID || '31337', 10);
+
+// Pilih chain berdasarkan env
+const chain = chainId === 11155111 ? sepolia : hardhat;
+
+// === CLIENT BACA (Gratis, tidak butuh private key) ===
+export const publicClient = createPublicClient({
+    chain,
+    transport: http(process.env.RPC_URL),
+});
+
+// === CLIENT TULIS (Butuh signer — hanya dipakai di Rebalancer!) ===
+export const walletClient = createWalletClient({
+    chain,
+    transport: http(process.env.SEPOLIA_URL),
+});
+
+export { chain, chainId };
